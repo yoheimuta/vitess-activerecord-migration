ced1f9e6907b03207cddb4a443908b6fdad10d26
Yohei Yoshimuta <yoheimuta@gmail.com>
2024-10-01 21:42:56 +0900

    Apply mysql.yaml before cluster.yaml

--- test/endtoend/migration_test.sh
+++ test/endtoend/migration_test.sh
@@ -14,15 +14,19 @@ echo "Apply operator.yaml"
 kubectl apply -f "test/endtoend/k8s/${VITESS_VERSION}/operator.yaml"
 check_pod_status_with_timeout "vitess-operator(.*)1/1(.*)Running(.*)"
 
+echo "Apply mysql.yaml"
+kubectl apply -f "test/endtoend/k8s/mysql.yaml"
+check_pod_status_with_timeout "mysql(.*)1/1(.*)Running(.*)" 2
+
+echo "Apply setup-replication-job.yaml"
+kubectl apply -f "test/endtoend/k8s/setup-replication-job.yaml"
+check_pod_status_with_timeout "setup-replication(.*)Completed(.*)"
+
 echo "Apply cluster.yaml"
 kubectl apply -f "test/endtoend/k8s/${VITESS_VERSION}/cluster.yaml"
 check_pod_status_with_timeout "example-zone1-vtctld(.*)1/1(.*)Running(.*)"
 check_pod_status_with_timeout "example-zone1-vtgate(.*)1/1(.*)Running(.*)"
 check_pod_status_with_timeout "example-etcd(.*)1/1(.*)Running(.*)" 3
-check_pod_status_with_timeout "mysql(.*)1/1(.*)Running(.*)" 2
-
-echo "Apply setup-replication-job.yaml"
-kubectl apply -f "test/endtoend/k8s/setup-replication-job.yaml"
 check_pod_status_with_timeout "example-vttablet-zone1(.*)1/1(.*)Running(.*)" 2
 
 echo "Start port-forwarding"
